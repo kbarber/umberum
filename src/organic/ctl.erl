304b2f256ba99632ab93ed7905330d76141b6960
Ken Barber <ken@bob.sh>
2010-08-15 01:04:10 +0100

    Add capability to stop in ologctl. Ported ctl.erl from kod to facilitate the RPC calls.

---
+++ src/organic/ctl.erl
@@ -0,0 +1,93 @@
+%% --------------------------
+%% @copyright 2010 Kenneth Barber
+%% @doc Organic control module.
+%%
+%% This module controls the organic erlang processes. It uses rpc techniques
+%% to run erlang commands on a running Organic node.
+%% 
+%% @end
+%% --------------------------
+-module(organic.ctl).
+
+-export([start/0,
+	 init/0,
+	 process/1
+	]).
+
+%%-----------------------------
+%% Module
+%%-----------------------------
+
+start() ->
+    case .init:get_plain_arguments() of
+	[SNode | Args] ->
+	    SNode1 = case .string:tokens(SNode, "@") of
+			 [_Node, _Server] ->
+			     SNode;
+			 _ ->
+			     case .net_kernel:longnames() of
+				 true ->
+				     SNode ++ "@" ++ .inet_db:gethostname() ++
+					 "." ++ .inet_db:res_option(domain);
+				 false ->
+				     SNode ++ "@" ++ .inet_db:gethostname();
+				 _ ->
+				     SNode
+			     end
+		     end,
+	    Node = list_to_atom(SNode1),
+	    Status = case .rpc:call(Node, ?MODULE, process, [Args]) of
+			 {badrpc, Reason} ->
+			     .io:format("Failed RPC connection to the node ~p: ~p~n",
+				    [Node, Reason]),
+			     %% TODO: show minimal start help
+			     ok;
+			 S ->
+			     S
+		     end,
+	    halt(Status);
+	_ ->
+	    %% TODO: print_usage() function
+	    halt()
+    end.
+
+init() ->
+    .ets:new(kod_ctl_cmds, [named_table, set, public]),
+    .ets:new(kod_ctl_host_cmds, [named_table, set, public]).
+
+
+%%-----------------------------
+%% Process
+%%-----------------------------
+
+process(["stop"]) ->
+    .init:stop(),
+    ok;
+
+process(["restart"]) ->
+    .init:restart(),
+    ok;
+
+process(["mnesia"]) ->
+    .io:format("~p~n", [.mnesia:system_info(all)]),
+    ok;
+
+process(["mnesia", "info"]) ->
+    .mnesia:info(),
+    ok;
+
+process(["tv"]) ->
+    .tv:start(),
+    ok;
+
+process(["etop"]) ->
+    .etop:start(),
+    ok;
+
+process(["mnesia", Arg]) when is_list(Arg) ->
+    case catch .mnesia:system_info(list_to_atom(Arg)) of
+	{'EXIT', Error} -> .io:format("Error: ~p~n", [Error]);
+	Return -> .io:format("~p~n", [Return])
+    end,
+    ok.
+
