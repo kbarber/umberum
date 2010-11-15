82258e65ea33aba54fd078fa0d5ab2cabe8739d6
Ken Barber <ken@bob.sh>
2010-11-15 08:51:56 +0000

    UMB-6: More unit tests for umberum.input.relp.relp_protocol.

--- engine/src/umberum/input/relp/relp_protocol_tests.erl
+++ engine/src/umberum/input/relp/relp_protocol_tests.erl
@@ -38,3 +38,28 @@ basic_2_test() ->
     ),
 
     ?assertEqual(Expected, Returned).
+
+basic_3_test() ->
+    Test = "basic_3",
+    Input = <<"1 open 85 relp_version=0\nrelp_software=librelp,1.0.0,http://librelp.adiscon.com\ncommands=syslog\n">>,
+    Expected = [{relp, 1, open, 85, <<"relp_version=0\nrelp_software=librelp,1.0.0,http://librelp.adiscon.com\ncommands=syslog">>}],
+    Returned = relp_protocol:decode(Input),
+
+    ?debugFmt("~nTest: ~p~nInput: ~p~nExpected: ~p~nReturned: ~p~n",
+        [ Test, Input, Expected, Returned ]
+    ),
+
+    ?assertEqual(Expected, Returned).
+
+multi_1_test() ->
+    Test = "multi_1",
+    Input = <<"3 syslog 221 <6>2010-11-14T02:16:37.077767+00:00 obelisk kernel: [898221.113355] iptables(INPUT-DROP): IN=br1 OUT= MAC= SRC=10.22.100.1 DST=10.22.100.255 LEN=156 TOS=0x00 PREC=0x00 TTL=64 ID=0 DF PROTO=UDP SPT=17500 DPT=17500 LEN=136 \n4 syslog 221 <6>2010-11-14T02:17:07.089755+00:00 obelisk kernel: [898251.127674] iptables(INPUT-DROP): IN=br1 OUT= MAC= SRC=10.22.100.1 DST=10.22.100.255 LEN=156 TOS=0x00 PREC=0x00 TTL=64 ID=0 DF PROTO=UDP SPT=17500 DPT=17500 LEN=136 \n5 syslog 224 <6>2010-11-14T02:18:37.149250+00:00 obelisk kernel: [898341.185822] iptables(INPUT-DROP): IN=br0 OUT= MAC= SRC=192.168.1.69 DST=255.255.255.255 LEN=156 TOS=0x00 PREC=0x00 TTL=64 ID=0 DF PROTO=UDP SPT=17500 DPT=17500 LEN=136 \n6 syslog 222 <6>2010-11-14T02:18:37.149268+00:00 obelisk kernel: [898341.186234] iptables(INPUT-DROP): IN=br0 OUT= MAC= SRC=192.168.1.69 DST=192.168.1.255 LEN=156 TOS=0x00 PREC=0x00 TTL=64 ID=0 DF PROTO=UDP SPT=17500 DPT=17500 LEN=136 \n7 syslog 221 <6>2010-11-14T02:18:37.149276+00:00 obelisk kernel: [898341.186389] iptables(INPUT-DROP): IN=br1 OUT= MAC= SRC=10.22.100.1 DST=10.22.100.255 LEN=156 TOS=0x00 PREC=0x00 TTL=64 ID=0 DF PROTO=UDP SPT=17500 DPT=17500 LEN=136 \n">>,
+    Expected = [{relp, 3, syslog, 221, <<"<6>2010-11-14T02:16:37.077767+00:00 obelisk kernel: [898221.113355] iptables(INPUT-DROP): IN=br1 OUT= MAC= SRC=10.22.100.1 DST=10.22.100.255 LEN=156 TOS=0x00 PREC=0x00 TTL=64 ID=0 DF PROTO=UDP SPT=17500 DPT=17500 LEN=136 ">>},{relp, 4, syslog, 221, <<"<6>2010-11-14T02:17:07.089755+00:00 obelisk kernel: [898251.127674] iptables(INPUT-DROP): IN=br1 OUT= MAC= SRC=10.22.100.1 DST=10.22.100.255 LEN=156 TOS=0x00 PREC=0x00 TTL=64 ID=0 DF PROTO=UDP SPT=17500 DPT=17500 LEN=136 ">>},{relp, 5, syslog, 224, <<"<6>2010-11-14T02:18:37.149250+00:00 obelisk kernel: [898341.185822] iptables(INPUT-DROP): IN=br0 OUT= MAC= SRC=192.168.1.69 DST=255.255.255.255 LEN=156 TOS=0x00 PREC=0x00 TTL=64 ID=0 DF PROTO=UDP SPT=17500 DPT=17500 LEN=136 ">>},{relp, 6, syslog, 222, <<"<6>2010-11-14T02:18:37.149268+00:00 obelisk kernel: [898341.186234] iptables(INPUT-DROP): IN=br0 OUT= MAC= SRC=192.168.1.69 DST=192.168.1.255 LEN=156 TOS=0x00 PREC=0x00 TTL=64 ID=0 DF PROTO=UDP SPT=17500 DPT=17500 LEN=136 ">>},{relp, 7, syslog, 221, <<"<6>2010-11-14T02:18:37.149276+00:00 obelisk kernel: [898341.186389] iptables(INPUT-DROP): IN=br1 OUT= MAC= SRC=10.22.100.1 DST=10.22.100.255 LEN=156 TOS=0x00 PREC=0x00 TTL=64 ID=0 DF PROTO=UDP SPT=17500 DPT=17500 LEN=136 ">>}],
+    Returned = relp_protocol:decode(Input),
+
+    ?debugFmt("~nTest: ~p~nInput: ~p~nExpected: ~p~nReturned: ~p~n",
+        [ Test, Input, Expected, Returned ]
+    ),
+
+    ?assertEqual(Expected, Returned).
+
